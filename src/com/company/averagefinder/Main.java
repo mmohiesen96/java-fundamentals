fcb5b9d086c8a715a4e5170abe909c85ad72f69c
mmohiesen96 <mmohiesen996@gmail.com>
2021-06-27 13:35:22 +0300

    Test

--- src/com/company/averagefinder/Main.java
+++ src/com/company/averagefinder/Main.java
@@ -1,8 +1,24 @@
 package com.company.averagefinder;
 
 public class Main {
-
+    public static String pluralize (String s , int count) {
+        if(count < 0){
+            return "Invalid Number";
+        }
+        else if (count == 1){
+            return  s;
+        }
+        return s + "s";
+    }
     public static void main(String[] args) {
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
 
     }
 }
